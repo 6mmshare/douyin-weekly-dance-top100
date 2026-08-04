8358ad77f0dbfc2055780c35b1fa880ae69c37e5
6mmshare <96071215+6mmshare@users.noreply.github.com>
2026-08-04 19:41:18 +0800

    Add visible pet Top100 launcher

---
+++ douyin_pet_dance_top100/01_运行宠物Top100_前台.bat
@@ -0,0 +1,8 @@
+@echo off
+chcp 65001 >nul
+cd /d "%~dp0"
+call "%~dp0_使用上一级环境.bat"
+if errorlevel 1 (pause&exit /b 1)
+title Douyin Pet Dance Top100 Visible
+"%PYTHON%" pet_weekly_ranker.py run --visible
+pause
