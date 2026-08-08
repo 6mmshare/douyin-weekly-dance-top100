c001ddc5efb6b0949aa8c9ad41a838c8a87428fa
6mmshare <96071215+6mmshare@users.noreply.github.com>
2026-08-08 13:04:25 +0800

    Add women Top100 collection shortcut

---
+++ 02_小姐姐榜_运行采集Top100.cmd
@@ -0,0 +1,8 @@
+@echo off
+chcp 65001 >nul
+setlocal
+cd /d "%~dp0"
+set "PY=%~dp0.venv\Scripts\python.exe"
+if not exist "%PY%" (echo [ERROR] .venv Python not found.&pause&exit /b 1)
+"%PY%" "%~dp0weekly_dance_ranker.py" run --visible
+pause
